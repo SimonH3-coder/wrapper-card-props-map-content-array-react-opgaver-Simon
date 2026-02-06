a2140ada5dbb6aa278318ac5fa1040081b98ec64
SimonH3-coder <simo16f4@elev.techcollege.dk>
2026-02-06 17:02:13 +0100

    style: format Cardcomponent for improved readability

--- src/components2/Cardcomponent.tsx
+++ src/components2/Cardcomponent.tsx
@@ -3,10 +3,11 @@ import { Cardbodycomponent } from "./Cardbodycomponent";
 import { CardFootercomponent } from "./CardFootercomponent";
 
 export const Cardcomponent = ({ cardData }) => {
-    return (
-        <div className="card">
-            <Cardheadercomponent text={cardData.header}/>
-            <Cardbodycomponent content={cardData.body}/>
-            <CardFootercomponent content={cardData.footer}/>
-
-        </div>
+  return (
+    <div className="card">
+      <Cardheadercomponent text={cardData.header} />
+      <Cardbodycomponent content={cardData.body} />
+      <CardFootercomponent content={cardData.footer} />
+    </div>
+  );
+};
